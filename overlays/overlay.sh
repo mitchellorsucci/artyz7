ec3289a067d9220749fcf8ab48ce90a77a2b7efe
Mitchell Orsucci <mitchellorsucci@gmail.com>
2017-08-25 13:33:33 -0700

    Split overlay.sh into two different files. Overlay.sh now loads and unloads overlays only. bitstream.sh now handles the behavior of loading the bitstream file

--- overlays/overlay.sh
+++ overlays/overlay.sh
@@ -2,15 +2,15 @@
 
 
 DTBO="$2"
-BIN="$3"
+
 echo
 echo
 echo -e "\tOverlay File ------> $DTBO"
-echo -e "\tFPGA Bit file ------> $BIN"
+
 echo
 
 
-start() {
+load() {
 	# Check to see if the Overlay and bit files are present in /lib/firmware
 	if [ -e /lib/firmware/$DTBO ]; then
 		echo -e "\tFound the .dtbo file"
@@ -19,12 +19,6 @@ start() {
 		exit 1
 	fi
 
-	if [ -e /lib/firmware/$BIN ]; then
-		echo -e "\tFound the FPGA bitstream file"
-	else
-		echo "The specified bitstream file is not present in /lib/firmware"
-		exit 1
-	fi
 
 	echo -e "\t**********************************************"
 	echo -e "\t          Loading Overlay $DTBO               "
@@ -55,16 +49,14 @@ start() {
 	fi
 
 
+	sleep 1
 	echo $DTBO > /config/device-tree/overlays/$DTBO/path
 	echo -e "\tOverlay Loaded"
-	sleep 2	
-	cat /lib/firmware/$BIN > /dev/xdevcfg
-	echo -e "\tBitstream Loaded"
 	echo
 	
 }
 
-stop() {
+unload() {
 	echo -e "\t**********************************************"
 	echo -e "\t          Unloading Overlay $DTBO             "
 	echo -e "\t**********************************************"
@@ -82,16 +74,16 @@ stop() {
 }
 
 case "$1" in
-	start)
-		start
+	load)
+		load
 		;;
-	stop)
-		stop
+	unload)
+		unload
 		;;
 	*)	
-		echo -e "\tUsage: $0 (start | stop) ---- 0verlay file ---- bitstream file"
-		echo -e "\tsuch as $0 start some_overlay.dtbo some_bitstream.bin"
-		echo -e "\tThe .dtbo and .bin files should both be in /lib/firmware"
+		echo -e "\tUsage: $0 (load | unload) ---- 0verlay file"
+		echo -e "\tsuch as $0 start some_overlay.dtbo"
+		echo -e "\tThe .dtbo file should both be in /lib/firmware"
 		exit 1
 esac
 
